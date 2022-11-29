19b43d1e945cbde0acedba25fc4ab2e8fd0c90af
penguin212 <dhuber06@gmail.com>
2022-11-28 22:27:51 -0800

    Implemented pipeline array, added data dicts

--- Main.py
+++ Main.py
@@ -14,11 +14,12 @@ class Main:
         self.green = GreenLight()
         self.april = AprilTags()
         self.localizer = Localization()
+        self.data = dict()
 
         self.pipelines = [self.green, self.localizer]
 
         # example source
-        self.turretcam = Source(0,    0, 0 ,1,   0, 0, 0)
+        self.turretcam = Source(0,    0, 0 , 1,   0, 0, 0)
         # port 0
         # raised by 1 unit (feet probably?)
         # pointed straight forward
@@ -28,14 +29,21 @@ class Main:
     
     def run(self):
         
+        data = dict()
+
+        for i in range(len(self.sources)):
+            data[self.sources[i]] = dict()
+            img = self.sources[i].get_frame()
+            for pipeline in self.pipelines:
+                data[self.sources[i]][pipeline] = (pipeline.get(img))
+        
         greenlights = []
         apriltags = []
 
-        for source in self.sources:
-            img = source.get_frame()
-            greenlights.append((source, self.green.get(img)))
-            apriltags.append((source, self.april.get(img)))
-            
+        for source in data:
+            greenlights.append((source, data[source][self.green]))
+            apriltags.append((source, data[source][self.april]))
+
 
 
         location = self.localizer.localize(greenlights, apriltags)
