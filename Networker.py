fae84160ffe0fef1a9265d6eb9b257099913c9ed
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-11-30 11:58:08 -0800

    PUB/SUB server

--- Networker.py
+++ Networker.py
@@ -1,19 +1,18 @@
 import time
 import zmq
 
-SERVER_IP = "tcp://localhost:5000"
+SERVER_IP = "tcp://*:5000"
 
 context = zmq.Context()
 
-print(f"Connecting a REQ client to {SERVER_IP}")
-socket = context.socket(zmq.REQ)
-socket.connect(SERVER_IP)
+print(f"Connecting a PUB server to {SERVER_IP}")
+socket = context.socket(zmq.PUB)
+socket.bind(SERVER_IP)
 
 while True:
     # Send JSON data to RIO
-    socket.send(b"{x: 3.5, y: 4.0, z: 1.203927}")
+    data = "{x: 3.5, y: 4.0, z: 1.203927}"
+    print(f"Sending data: {data}")
+    socket.send_string(data)
 
     time.sleep(1)
-
-    message = socket.recv()
-    print("Received reply %s" % message)
