572871b78c466780e977ec3fcdb01f09112322a6
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-11-30 21:49:16 -0800

    High volume PUB/SUB test

--- Networker.py
+++ Networker.py
@@ -1,3 +1,5 @@
+import json
+from random import Random
 import time
 import zmq
 
@@ -9,10 +11,19 @@ print(f"Connecting a PUB server to {SERVER_IP}")
 socket = context.socket(zmq.PUB)
 socket.bind(SERVER_IP)
 
-while True:
-    # Send JSON data to RIO
-    data = "{x: 3.5, y: 4.0, z: 1.203927}"
-    print(f"Sending data: {data}")
-    socket.send_string(data)
+random = Random()
 
-    time.sleep(1)
+while True:
+    data = {
+        "x": 12.12507 * random.random(),
+        "y": 3.2394682 * random.random(),
+        "z": 5.238 * random.random(),
+        "ts": time.time_ns() // 1000000  # Current time in ms
+    }
+
+    # Send JSON data to RIO
+    message = json.dumps(data)
+    print(f"Sending data: {message}")
+    socket.send_string(message)
+
+    time.sleep(0.01)
