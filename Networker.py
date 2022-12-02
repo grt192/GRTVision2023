22b17fa5078ab559d17312efc59a66cb04128248
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-12-02 13:21:51 -0800

    Handle RIO reconnects, poll for messages

--- Networker.py
+++ Networker.py
@@ -5,6 +5,7 @@ import zmq
 
 LOCAL_DEBUG = True
 SERVER_IP = "tcp://*:5000" if LOCAL_DEBUG else "tcp://10.1.92.94:5000"
+RIO_IDENT = b"RIO"
 
 context = zmq.Context()
 
@@ -22,10 +23,15 @@ while True:
         "ts": int(time.time() * 1000)  # Current time in ms
     }
 
+    # Poll socket for inbound messages
+    if socket.poll(0):
+        RIO_IDENT = socket.recv()  # Store the identity payload in case the RIO has reconnected
+        print(f"Received: {socket.recv_string()}")
+
     # Send JSON data to RIO
     message = json.dumps(data)
     print(f"Sending data: {message}")
-    socket.send_string("RIO", flags=zmq.Flag.SNDMORE)  # Prefix with RIO identity string
+    socket.send(RIO_IDENT, flags=zmq.SNDMORE)  # Prefix with RIO identity string
     socket.send_string(message)
 
     time.sleep(0.01)
