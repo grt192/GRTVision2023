438a601fe09c61a83507f6731474db6adc5cc006
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-12-02 19:08:07 -0800

    Multiprocessing and data-queuing

--- Networker.py
+++ Networker.py
@@ -2,36 +2,51 @@ import json
 from random import Random
 import time
 import zmq
+from multiprocessing import Process, SimpleQueue
 
 LOCAL_DEBUG = True
 SERVER_IP = "tcp://*:5000" if LOCAL_DEBUG else "tcp://10.1.92.94:5000"
-RIO_IDENT = b"RIO"
 
-context = zmq.Context()
 
-print(f"Connecting a ROUTER to {SERVER_IP}")
-socket = context.socket(zmq.ROUTER)
-socket.bind(SERVER_IP)
+# A networker that sends and receives data from the RoboRIO over a ROUTER/DEALER socket.
+# Data is queued to be sent via the `data_queue`, and is broadcast as fast as it arrives.
+def networker(data_queue: SimpleQueue):
+    context = zmq.Context()
 
-random = Random()
+    print(f"Connecting a ROUTER to {SERVER_IP}")
+    socket = context.socket(zmq.ROUTER)
+    socket.bind(SERVER_IP)
 
-while True:
-    data = {
-        "x": 12.12507 * random.random(),
-        "y": 3.2394682 * random.random(),
-        "z": 5.238 * random.random(),
-        "ts": int(time.time() * 1000)  # Current time in ms
-    }
+    rio_ident = b"RIO"
 
-    # Poll socket for inbound messages
-    if socket.poll(0):
-        RIO_IDENT = socket.recv()  # Store the identity payload in case the RIO has reconnected
-        print(f"Received: {socket.recv_string()}")
+    while True:
+        # Poll socket for inbound messages
+        if socket.poll(0):
+            rio_ident = socket.recv()  # Store the identity payload in case the RIO has reconnected
+            print(f"Received: {socket.recv_string()}")
 
-    # Send JSON data to RIO
-    message = json.dumps(data)
-    print(f"Sending data: {message}")
-    socket.send(RIO_IDENT, flags=zmq.SNDMORE)  # Prefix with RIO identity string
-    socket.send_string(message)
+        # If there's data in the queue, send it to the RIO
+        if not data_queue.empty():
+            message = json.dumps(data_queue.get())
+            print(f"Sending data: {message}")
+            socket.send(rio_ident, flags=zmq.SNDMORE)  # Prefix with RIO identity string
+            socket.send_string(message)
 
-    time.sleep(0.01)
+
+if __name__ == '__main__':
+    random = Random()
+    data_queue = SimpleQueue()
+
+    networker_process = Process(target=networker, args=(data_queue,), daemon=True)
+    networker_process.start()
+
+    # Test the networker by continually sending semi-random data
+    while True:
+        data = {
+            "x": 12.12507 * random.random(),
+            "y": 3.2394682 * random.random(),
+            "z": 5.238 * random.random(),
+            "ts": int(time.time() * 1000)  # Current time in ms
+        }
+        data_queue.put(data)
+        time.sleep(0.01)
