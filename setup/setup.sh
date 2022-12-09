4a518e2444a9f7c9b3f2c4491438fccd154dc1e2
Ethan Liang <e3lcool@gmail.com>
2022-12-09 15:16:34 -0800

    Enabled streaming setup script, tweaked all setup scripts to work on freshly flashed card

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,18 +1,31 @@
 # cwd to this script's directory
 cd "$(dirname "$0")"
+# allow all scripts in this directory to be executed
+sudo chmod -R u+x ./
 
 # update/upgrade all apt packages
-sudo apt-get -y update 
-sudo apt-get -y dist-upgrade
+sudo apt-get update 
+sudo apt-get upgrade
 
 # Install universal tools
 sudo apt-get -y install python3-pip
 
 # setup jetson's ip
+echo "------------------------"
+echo "IP SETUP -------------------------------------------------- IP SETUP"
 ./setup_ip.sh
 
 # setup networking
+echo "------------------------"
+echo "NETWORKING SETUP ---------------------------------------------- NETWORKING SETUP"
 ./setup_networking.sh
 
 # setup apriltags
+echo "------------------------"
+echo "APRILTAG SETUP ---------------------------------------------- APRILTAG SETUP"
 ./setup_apriltags.sh
+
+# setup streaming
+echo "------------------------"
+echo "STREAMING SETUP ---------------------------------------------- STREAMING SETUP"
+./setup_streaming.sh
--- setup/setup_networking.sh
+++ setup/setup_networking.sh
@@ -1,3 +1,6 @@
+# zeromq install dependencies
+pip3 install packaging
+
 # Install ZeroMQ
 # https://pypi.org/project/pyzmq
 sudo apt-get install -y libzmq3-dev
