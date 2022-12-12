30883e4c1bdbbe33992746fe2b9c7d900d368dc8
Ethan Liang <e3lcool@gmail.com>
2022-12-12 15:36:20 -0800

    evenm ore working???

--- tests/stream_test.py
+++ tests/stream_test.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
 
-    gstream = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink port=5000"
+    print(str(width) + " " + str(height))
+
+    gstream = "appsrc ! videoconvert ! videoscale ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 !  videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink port=5000"
 
     out = cv2.VideoWriter(gstream, 0, 30.0, (int(width), int(height)))
 
