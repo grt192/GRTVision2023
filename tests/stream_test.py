9b2f2841e630757450a917991cfdbe6d9adfe00e
grt <grt@grt.localdomain>
2022-12-16 17:29:58 -0800

    WORKING STREAMING!

--- tests/stream_test.py
+++ tests/stream_test.py
@@ -18,11 +18,12 @@ if __name__ == '__main__':
 
     print(str(width) + " " + str(height))
 
-    gstream = "appsrc ! videoconvert ! videoscale ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 !  videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=192.168.55.1 port=5000"
+    gstream = "appsrc ! videoconvert ! videoscale ! video/x-raw,width=640,height=480, framerate=30/1 ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink port=49999 host=192.168.55.100" 
 
     out = cv2.VideoWriter(gstream, 0, 30.0, (int(width), int(height)))
 
     while True:
         _, frame = cap.read()
+        cv2.rectangle(frame, (10,10), (100,100), (255,0,0), 2)
         out.write(frame)
 
